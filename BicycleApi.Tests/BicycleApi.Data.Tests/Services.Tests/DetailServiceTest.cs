58fc69132755f1ce746e5eb9e6d03a24d540bc75
vova-kuharchyk <vovafalkon@gmail.com>
2021-05-14 10:45:50 +0300

    add upsert test method

--- BicycleApi.Tests/BicycleApi.Data.Tests/Services.Tests/DetailServiceTest.cs
+++ BicycleApi.Tests/BicycleApi.Data.Tests/Services.Tests/DetailServiceTest.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using BicycleApi.Data.Interfaces;
 using BicycleApi.Data.Services;
 using BicycleApi.DBData.Entities;
@@ -22,13 +22,17 @@ namespace BicycleApi.Tests.BicycleApi.Data.Tests.Services.Tests
 		public void GetShouldReturnAllAsyncTest()
 		{
 			List<Detail> fakeDetails = new List<Detail>();
+
 			var mockIRepository = new Mock<IRepository<Detail>>();
 			var mockBrandService = new Mock<IBrandService>();
 			var mockCountryService = new Mock<ICountryService>();
+
 			mockIRepository.Setup(x => x.GetAsync().Result).Returns(() => fakeDetails.AsQueryable());
 
 			fakeDetails.Add(new Detail() { Type = DetailType.Coins, Color = "asd" });
+
 			var detailService = new DetailService(mockIRepository.Object, mockCountryService.Object, mockBrandService.Object);
+
 			var result = detailService.GetAsync().Result.ToList();
 
 			result.Count.Should().Be(1);
@@ -38,9 +42,11 @@ namespace BicycleApi.Tests.BicycleApi.Data.Tests.Services.Tests
 		public void GetShouldReturnAllTest()
 		{
 			List<Detail> fakeDetails = new List<Detail>();
+
 			var mockIRepository = new Mock<IRepository<Detail>>();
 			var mockBrandService = new Mock<IBrandService>();
 			var mockCountryService = new Mock<ICountryService>();
+
 			mockIRepository.Setup(x => x.GetWithInclude(x => x.Country, x => x.Brand)).Returns(() => fakeDetails.AsQueryable());
 
 			fakeDetails.Add(new Detail() { Type = DetailType.Coins, Color = "asd" });
@@ -53,15 +59,21 @@ namespace BicycleApi.Tests.BicycleApi.Data.Tests.Services.Tests
 		[Fact]
 		public async Task GetShouldReturnById()
 		{
-			Detail fakeDetail = new Detail() { Id = 2, Color = "asd" };
+			var fakeDetail = new Detail() { Id = 2, Color = "asd" };
+			var fakeDetails = new List<Detail> { fakeDetail };
+
 			var mockIRepository = new Mock<IRepository<Detail>>();
 			var mockBrandService = new Mock<IBrandService>();
 			var mockCountryService = new Mock<ICountryService>();
-			mockIRepository.Setup(x => x.GetByIdAsync(1).Result).Returns(fakeDetail);
+
+			mockIRepository.Setup(x => x.GetByIdAsync(2).Result).Returns(fakeDetail);
+			mockIRepository.Setup(x => x.GetWithInclude(detail => detail.Country, detail => detail.Brand)).Returns(fakeDetails);
 
 			var detailService = new DetailService(mockIRepository.Object, mockCountryService.Object, mockBrandService.Object);
-			var result = await detailService.GetByIdAsync(1);
-			Assert.Equal(1, result.Id);
+
+			var result = await detailService.GetByIdAsync(2);
+
+			Assert.Equal(2, result.Id);
 		}
 
 
@@ -69,13 +81,47 @@ namespace BicycleApi.Tests.BicycleApi.Data.Tests.Services.Tests
 		public async Task RemoveAsyncShouldBeExecutedTest()
 		{
 			var fakeDetail = new Detail();
+
 			var mockIRepository = new Mock<IRepository<Detail>>();
 			var mockBrandService = new Mock<IBrandService>();
 			var mockCountryService = new Mock<ICountryService>();
 
 			var detailService = new DetailService(mockIRepository.Object, mockCountryService.Object, mockBrandService.Object);
 			await detailService.RemoveAsync(fakeDetail);
+
 			mockIRepository.Verify(service => service.RemoveAsync(fakeDetail), Times.Once);
 		}
+
+		[Fact]
+		public async Task UpsertAsyncShouldCreateTest()
+		{
+			var fakeCountry = new Country() { Name = "Country" };
+			var fakeBrand = new Brand() { Name = "Brand" };
+			var fakeDetail = new Detail() { Id = 2, Color = "asd", Country = fakeCountry, Brand = fakeBrand };
+			var fakeDetails = new List<Detail> { fakeDetail };
+
+			var mockIRepository = new Mock<IRepository<Detail>>();
+			var mockBrandService = new Mock<IBrandService>();
+			var mockCountryService = new Mock<ICountryService>();
+
+			mockIRepository.Setup(x => x.GetByIdAsync(fakeDetail.Id).Result).Returns(fakeDetail);
+			mockIRepository.Setup(x => x.GetWithInclude(x => x.Country, x => x.Brand)).Returns(fakeDetails);
+			mockIRepository.Setup(x => x.UpdateAsync(fakeDetail).Result).Returns(fakeDetail);
+			mockIRepository.Setup(x => x.CreateAsync(fakeDetail).Result).Returns(fakeDetail);
+
+			mockCountryService.Setup(x => x.UpsertAsync(fakeCountry.Name).Result).Returns(fakeCountry);
+
+			mockBrandService.Setup(x => x.UpsertAsync(fakeBrand.Name).Result).Returns(fakeBrand);
+
+			var detailService = new DetailService(mockIRepository.Object, mockCountryService.Object, mockBrandService.Object);
+
+			var result = await detailService.UpsertAsync(fakeDetail);
+
+			Assert.Equal(fakeDetail.Id, result.Id);
+			Assert.Equal(fakeDetail.Color, result.Color);
+			Assert.Equal(fakeDetail.Material, result.Material);
+			Assert.Equal(fakeDetail.Type, result.Type);
+		}
+		//TODO more tests to aim full test coverage
 	}
 }
